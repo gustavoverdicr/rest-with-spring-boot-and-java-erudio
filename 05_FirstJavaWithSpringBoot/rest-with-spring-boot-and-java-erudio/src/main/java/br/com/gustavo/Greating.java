775abd04ebcc3683b633de5c5a2541502450fcbd
Gustavo Verdi <gvcristofolini@furb.br>
2024-05-28 10:32:59 -0300

    Frist Steps in Java with Spring Boot

---
+++ 05_FirstJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/gustavo/Greating.java
@@ -0,0 +1,25 @@
+package br.com.gustavo;
+
+public class Greating {
+      private final long id;
+      private final String content;
+	
+      public Greating(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+      
+      
+      
+      
+}
